86050b66f2a1e3c5f0f306af4b08cef7acc4c790
Ayman428 <asayed@tanfeethi.com.sa>
2025-04-20 09:11:40 +0200

    Update Container.tsx

--- src/components/once/Container.tsx
+++ src/components/once/Container.tsx
@@ -10,26 +10,26 @@ type Props = {
 const Container = ({ title, buttonText }: Props) => {
     return (
         <div
-            className="relative bg-cover bg-center w-full h-[465px]"
+            className="relative bg-cover w-full h-[465px]"
             style={{
                 backgroundImage: `url(${CTA_BG})`,
                 backgroundSize: "cover",
-                backgroundPosition: "center",
+                backgroundPosition: "top left",
             }}
         >
             <div className="absolute inset-0 bg-gradient-to-r from-[#071839]/50 to-[#071839]" />
             
             <WrapperContainer>
-                <div data-aos="fade-right" className="relative z-10 flex flex-col md:flex-row items-center justify-between px-8 md:px-16">
-                    <h2 data-aos="fade-right" className="text-2xl md:text-3xl font-bold">
+                <div data-aos="fade-right" className="relative z-10 flex flex-col justify-between px-8 md:px-16">
+                    <h2 data-aos="fade-right" className="text-2xl md:text-3xl font-bold text-white">
                         {title}
                     </h2>
 
-                    <div data-aos="fade-left" className="mt-6 md:mt-0">
+                    <div data-aos="fade-left" className="my-4">
                         <Link to="/our-approach">
                             <button
                                 data-aos="fade-left"
-                                className="px-6 py-3 bg-primary-7 text-white rounded-lg font-medium hover:bg-[#228181] transition-all"
+                                className="px-6 py-3 bg-[#6E98ED] text-white rounded-lg font-medium hover:bg-[#228181] transition-all"
                             >
                                 {buttonText}
                             </button>
